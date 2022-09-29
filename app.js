b61f05f5efdacdba104d7c6db632ae8b221bb68c
jeoneunseo <95198414+jeoneunseo@users.noreply.github.com>
2022-09-30 00:12:03 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const age = 96;
-function calculateKrAge(afeOfForeigner){
-    afeOfForeigner + 2;
-    return "hello";
+//예제
+const age = parseInt(prompt("how old are you?"));
+    //prompt 창을 띄워서 결과값을 받을 수 있음
+    //parseInt 문자를 숫자로 변환해줌
+if(isNaN(age)){
+    console.log("please write a number.");
+} else{
+    console.log("Thank you for writing your age.");
 }
-
-const KrAge = calculateKrAge(age);
-
-console.log(KrAge);
+    //isNaN 숫자면 true, 아니면 false를 반환함
